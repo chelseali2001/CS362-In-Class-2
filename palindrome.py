22420218aa819a8088e4d48632b6ceac3e0acfbb
Chelsea Li <chelseali2001@gmail.com>
2021-05-13 21:41:30 -0700

    created both programs

---
+++ palindrome.py
@@ -0,0 +1,5 @@
+def palin(s):
+    if s == s[::-1]:
+        return True
+    
+    return False
---
+++ wordCount.py
@@ -0,0 +1,5 @@
+def count(s):
+    arr = s.split(" ")
+    return len(arr)
+
+print(count("This is an activity"))
