5916911ceb914587aef2a4cade34d1a250bb4cd9
Chelsea Li <chelseali2001@gmail.com>
2021-05-14 23:36:12 -0700

    changed some programs

--- test_palindromeUnittest.py
+++ test_palindromeUnittest.py
@@ -4,6 +4,7 @@ import palindrome
 class SimpleTest(unittest.TestCase):
     def test_calculator(self):
         self.assertEqual(palindrome.palin("racecar"), True)
+        print("Palindrome test case 1: 'radar' == True --> Passed")
 
     def test_calculator0(self):
         self.assertEqual(palindrome.palin("Racecar"), True)
